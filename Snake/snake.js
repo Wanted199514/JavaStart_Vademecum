136735bbb96f576e35cb92e8226a58725be8b89e
Volodymyr, Dubovskyi <gdd0070@gmail.com>
2021-03-06 08:36:38 +0100

    Add styles

--- Snake/snake.js
+++ Snake/snake.js
@@ -1,2 +1,8 @@
-var plansza = document.getElementById("toMoje");
+var plansza = document.getElementById("plotno");
 console.log(plansza);
+
+var kontekst = plotno.getContext("2d");
+kontekst.strokeStyle = "rgb(255,0,0)";
+kontekst.fillStyle =  "rgb(0,255,0)";
+kontekst.strokeRect(100,100,100,100);
+kontekst.fillRect(700,30,30,30);
